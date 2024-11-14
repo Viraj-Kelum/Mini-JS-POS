aa5f63c3706fff2c7e57d4fd8cb03ae5aebadb26
VirajKelum <workmail12311@gmail.com>
2024-11-15 00:36:07 +0530

    12:05 A.M

--- js/main.js
+++ js/main.js
@@ -9,18 +9,27 @@
 
 // Register User
 
-let userArr=[];
+let userArr = [];
+let customerArr = [];
 
-class User{
-    constructor(fullName,email,password) {
+class User {
+    constructor(fullName, email, password) {
         this.fullName = fullName;
         this.email = email;
-        this.password=password;
+        this.password = password;
     }
 }
 
+class Customer {
+    constructor(name, address, salary, nic) {
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+        this.nic = nic;
+    }
+}
 
-const createUser =()=>{
+const createUser = () => {
 
 
     const fullName = $('#full-name').val();
@@ -28,12 +37,11 @@ const createUser =()=>{
     const password = $('#register-password').val();
 
 
+    let createdUser = new User(fullName, email, password);
 
-    let createdUser= new User(fullName , email , password);
+    let existsUserData = userArr.find(data => data.email === email);
 
-    let existsUserData =userArr.find(data=>data.email===email);
-
-    if(existsUserData){
+    if (existsUserData) {
         alert('User email already ')
         return;
     }
@@ -47,24 +55,73 @@ const createUser =()=>{
 
 // Login User
 
-const login=()=>{
+const login = () => {
     const email = $('#email').val();
     const password = $('#password').val();
 
     let existingUserData = userArr.find(data => data.email === email);
 
-    if(existingUserData){
-        if(existingUserData.password===password){
+    if (existingUserData) {
+        if (existingUserData.password === password) {
             //login => dashboard
             console.log('login success')
-        }else{
+        } else {
             alert('password is wrong');
 
         }
-    }else{
+    } else {
         alert('invalid email or password')
     }
 
 }
 
 // Login User
+
+
+//CRUD
+
+const createCustomer = () => {
+
+    const customer = new Customer(
+        $('#name').val(),
+        $('#address').val(),
+        parseFloat($('#name').val()),
+        $('#nic').val(),
+    );
+
+    let existsData = customerArr.find(data => data.nic === customer.nic);
+
+    if (existsData) {
+        alert('Customer Already exists');
+    }
+
+    customerArr.push(customer);
+}
+
+const findCustomer = (id) => {
+
+
+}
+
+
+const updateCustomer = () => {
+
+}
+
+const deleteCustomer = () => {
+
+    for (let i = 0; i < customerArr.length; i++) {
+        if (customerArr[i].nic === id) {
+            customerArr.splice(i, 1);
+            alert('deleted');
+            return;
+        }
+    }
+
+}
+
+const getAllCustomers = () => {
+
+}
+
+//CRUD
