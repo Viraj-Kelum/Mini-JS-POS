9afe66bfc41e48e911abf7614e3090810d90782c
VirajKelum <workmail12311@gmail.com>
2024-11-14 22:24:31 +0530

    Login and register Done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,70 @@
-const clearAndLoad = (element)=>{
+// const clearAndLoad = (element)=>{
+//
+//    let body =  $('body').empty();
+//
+//     let tempElement = $('#'+element);
+//     tempElement.appendTo(body);
+//
+// }
 
-   let body =  $('body').empty();
+// Register User
 
-    let tempElement = $('#'+element);
-    tempElement.appendTo();
+let userArr=[];
+
+class User{
+    constructor(fullName,email,password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password=password;
+    }
+}
+
+
+const createUser =()=>{
+
+
+    const fullName = $('#full-name').val();
+    const email = $('#register-email').val();
+    const password = $('#register-password').val();
+
+
+
+    let createdUser= new User(fullName , email , password);
+
+    let existsUserData =userArr.find(data=>data.email===email);
+
+    if(existsUserData){
+        alert('User email already ')
+        return;
+    }
+
+    userArr.push(createdUser);
+    console.log(userArr);
+}
+
+// Register User
+
+
+// Login User
+
+const login=()=>{
+    const email = $('#email').val();
+    const password = $('#password').val();
+
+    let existingUserData = userArr.find(data => data.email === email);
+
+    if(existingUserData){
+        if(existingUserData.password===password){
+            //login => dashboard
+            console.log('login success')
+        }else{
+            alert('password is wrong');
+
+        }
+    }else{
+        alert('invalid email or password')
+    }
 
 }
+
+// Login User
